51bbb37319accb55d9ca92882b88474308d8d3d0
Lysander Mealy <lysander-mbpro@lysanders-mbp.lan>
2021-10-15 17:51:42 -0400

    Initial commit

---
+++ Library/polaris.cpp
@@ -0,0 +1,7 @@
+#include "polaris.h"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ Testing/main.cpp
@@ -0,0 +1,6 @@
+#include <polaris.h>
+
+int main() {
+    hello();
+    return 0;
+}
