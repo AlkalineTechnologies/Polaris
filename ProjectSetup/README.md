1b46654250a57ee09cdbd60c1e0c597ff2b4a6c8
plasmarad <73721329+plasmarad@users.noreply.github.com>
2022-03-20 22:54:46 -0400

    Delete ProjectSetup directory

--- ProjectSetup/README.md
+++
@@ -1,1 +0,0 @@
-A simple GUI for developers to set up their projects for the polaris engine
--- ProjectSetup/config.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __CONFIG_H__
-#define __CONFIG_H__
-
-#define PROJECT_NAME        plasmarad
-#define PROJECT_PATH        .
-#define PROJECT_AUTHOR      me
-#define PROJECT_DESCRIPTION lol
-
-#endif // __CONFIG_H__
