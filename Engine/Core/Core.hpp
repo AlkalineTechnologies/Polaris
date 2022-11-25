920e1ae0c61a3b6e2409d8e6bfc023e59bbbbae1
plasmarad <userofgogle356@gmail.com>
2022-11-25 12:28:17 -0500

    structure (sync  commit)

--- Engine/Core/Core.hpp
+++
@@ -1,7 +0,0 @@
-//polaris header
-
-#ifndef __POLARIS_CORE_ENGINE_HPP__
-#define __POLARIS_CORE_ENGINE_HPP__
-
-#endif
-
--- Engine/Core/Polaris.cpp
+++
@@ -1,7 +0,0 @@
-#include <Include/Polaris.hpp>
-
-void Polaris::Exit(int _status){
-    // call destructor of the main entry function
-    
-    Polaris::Entry().~Instance();
-}
