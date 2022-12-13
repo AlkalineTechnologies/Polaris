acd36d57d59b6bdf8579570b3ea47ca749610815
plasmarad <userofgogle356@gmail.com>
2022-12-13 00:16:59 -0500

    updat. gitignore + README + Argument parser.

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Polaris
 > ![polaris_banner.png](polaris_LOGO_white_strip.png)
 
-***A framework for everyone! where your imagination is the limit.***
 
-## About
-Polaris is a framework that allows you to built anything you desire, whether that is a fancy GUI, to a simple 2D sidescroller, or a simple CLI application.
-all of this possible through the use of modules and plugins, which are developed by the community for the community. and all of this so you can develop your apps easier and more flexibly.
+# ***P O L A R I S***
+*Realiable application framework that allows you to swap components without having to 
+refactor your code.*
 
-> **Note** 
-> polaris is in its early stages and has only begun development
+## ***Introducing PCLT***
+`PCLT` is a command line tool that allows you to manage your polaris project, and swap components/modules.
--- core/Entry.cpp
+++ core/Entry.cpp
@@ -1,8 +1,38 @@
 #include "argparse.hpp"
+#include <string>
+#include <map>
+
+#include "arg_actions.hpp"
+
 
 int main(int argc, char** argv){
     argparse::ArgumentParser program("PCLT");
 
-
     // add arguments
+
+    program.add_argument("-n","--new")
+        .nargs(1)
+        .help("Initialize a new Polaris Project");
+        
+    program.add_argument("-i","--init")
+        .nargs(1) // Where to init the Workspace
+        .help("Initialize a new Polaris Module");
+
+    program.add_argument("-b","--build")
+        .nargs(1) // Where to build
+        .help("Build a Polaris Project");
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cout << err.what() << std::endl;
+        std::cout << program;
+        return 0;
+    }
+
+    Args_To_Act(program);
+
+    // if get "project" : ...
+    // else if get "module" : ...
 }
